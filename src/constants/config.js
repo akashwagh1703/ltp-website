4663d3b115a6f00ade3166bcf0d8c83023f4c02d
Akash Wagh <Akash.Wagh@esds.co.in>
2026-01-18 22:19:38 +0530

    get master

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,6 @@
 // App Configuration
 export const APP_CONFIG = {
-  DOWNLOAD_LINK: 'https://drive.google.com/file/d/1DZoX7w3u4iX8ZGZz5Zi4dCJFTQpWhZnN/view?usp=drive_link',
+  DOWNLOAD_LINK: 'https://drive.google.com/file/d/1GDWjk6T9mHuTaEs17LymHfaTBuBL6V6J/view?usp=sharing',
   CONTACT: {
     PHONE: '+91 7020527719',
     EMAIL: 'letsturfplay@gmail.com'
